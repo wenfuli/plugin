afe63951a05294a84021fb6709d8007e846f5900
GitHub Action <action@github.com>
2021-11-12 01:45:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/wenfuli/plugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wenfuli/plugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wenfuli.plugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wenfuli.plugin.MyBundle
 
 class MyApplicationService {
 
